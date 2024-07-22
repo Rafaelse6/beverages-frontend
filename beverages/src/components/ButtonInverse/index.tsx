2e14007843722f9c2450b995de6a2b1e57a5c111
Rafael Santos <rafa.05125@gmail.com>
2024-07-22 13:22:40 -0300

    Initial buttons

---
+++ beverages/src/components/ButtonInverse/index.tsx
@@ -0,0 +1,7 @@
+export default function ButtonInverse() {
+  return (
+    <div className="w-full p-7 h-10 rounded-md text-base font-bold flex justify-center items-center cursor-pointer border border-bec-color-btn-primary bg-bec-color-btn-inverse text-bec-color-btn-primary leading-none">
+      HOME
+    </div>
+  );
+}
---
+++ beverages/src/components/ButtonPrimary/index.tsx
@@ -0,0 +1,7 @@
+export default function ButtonPrimary() {
+  return (
+    <div className="w-full p-7 h-10 rounded-md text-base font-bold flex justify-center items-center cursor-pointer border-none bg-bec-color-btn-primary color: rgb(0 0 0)">
+      BUY
+    </div>
+  );
+}
