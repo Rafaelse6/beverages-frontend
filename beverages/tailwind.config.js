2723e346b17b76763498259cb90d4394456d136d
Rafael Santos <rafa.05125@gmail.com>
2024-07-23 11:13:25 -0300

    Setting more colors

--- beverages/tailwind.config.js
+++ beverages/tailwind.config.js
@@ -3,9 +3,18 @@ export const content = ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"];
 export const theme = {
   extend: {
     colors: {
-      "bec-color-bg-primary": "#e8e8e8",
+      "bec-color-bg-primary": "#E8E8E8",
       "bec-color-bg-secundary": "#00105F",
       "bec-color-bg-tertiary": "#E8BB00",
+
+      "bec-color-card-border": "#D9D9D9",
+      "bec-color-btn-primary": "#D19300",
+      "bec-color-card-bg": "#FFF",
+
+      "bec-color-btn-inverse": "#FFF",
+      "bec-color-font-primary": "#000",
+      
+      "beg-color-error": "#F33",
     },
   },
 };
