f1fabff231e6424c320bbcd2fcb8bca825a96957
Rafael Santos <rafa.05125@gmail.com>
2024-08-17 00:08:42 -0300

    Beverage as prop

--- beverages/src/components/ProductDetailsCard/index.tsx
+++ beverages/src/components/ProductDetailsCard/index.tsx
@@ -1,31 +1,29 @@
-import cocaCola from "../../assets/coca-cola.png";
+import { BeverageDTO } from "../../models/beverage";
 import ProductCategory from "../ProductCategory";
 
-export default function ProductDetailsCard() {
+type Props = {
+  beverage: BeverageDTO;
+};
+
+export default function ProductDetailsCard({ beverage }: Props) {
   return (
     <div className="border border-bec-color-card-border rounded bg-bec-color-card-bg mb-5 p-5 max-w-md mx-auto shadow-lg">
       <div className="p-2.5 flex justify-center border-b border-bec-color-card-border">
         <img
-          src={cocaCola}
-          alt="Computador"
+          src={beverage.imgUrl}
+          alt={beverage.name}
           className="w-56 h-auto object-contain"
         />
       </div>
       <div className="p-2.5 md:px-5 md:py-2.5">
         <h3 className="text-bec-color-font-secondary text-xl font-bold flex items-center">
-          R$ 2,00
+          R$ {beverage.price.toFixed(2)}
         </h3>
         <h4 className="text-bec-color-font-primary text-lg font-semibold shadow-md">
-          Coca-Cola
+          {beverage.name}
         </h4>
         <p className="text-bec-color-font-primary text-sm my-2.5 bg-gray-100 p-3 rounded-md">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          {beverage.description}
         </p>
         <div className="flex flex-wrap justify-center text-center">
           <ProductCategory name="Non alcoholic" />
--- beverages/src/routes/ProductDetails/index.tsx
+++ beverages/src/routes/ProductDetails/index.tsx
@@ -2,6 +2,26 @@ import ButtonInverse from "../../components/ButtonInverse";
 import ButtonPrimary from "../../components/ButtonPrimary";
 import HeaderClient from "../../components/HeaderClient/HeaderClient";
 import ProductDetailsCard from "../../components/ProductDetailsCard";
+import { BeverageDTO } from "../../models/beverage";
+
+const beverage: BeverageDTO = {
+  id: 2,
+  name: "Sprite",
+  description: "This soda is really good",
+  imgUrl:
+    "https://e7.pngegg.com/pngimages/79/169/png-clipart-sprite-easy-open-can-illustration-sprite-fizzy-drinks-coca-cola-fanta-beverage-can-fanta-food-aluminum-can.png",
+  price: 2.0,
+  categories: [
+    {
+      id: 1,
+      name: "Soda",
+    },
+    {
+      id: 3,
+      name: "Non alcoholic",
+    },
+  ],
+};
 
 export default function ProductDetails() {
   return (
@@ -9,7 +29,7 @@ export default function ProductDetails() {
       <HeaderClient />
       <main className="bg-bec-color-bg-primary min-h-screen">
         <section id="product-details-section" className="dsc-container py-8">
-          <ProductDetailsCard />
+          <ProductDetailsCard beverage={beverage} />
           <div className="flex justify-center gap-4 mt-6">
             <ButtonPrimary />
             <ButtonInverse />
