a4dc49d117f5b0370f716fc59786eea4271319a4
Rafael Santos <rafa.05125@gmail.com>
2024-08-27 11:29:21 -0300

    Links

--- beverages/src/components/HeaderClient/HeaderClient.tsx
+++ beverages/src/components/HeaderClient/HeaderClient.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import cartIcon from "../../assets/cart.svg";
 
 export default function HeaderClient() {
@@ -5,16 +6,20 @@ export default function HeaderClient() {
     <header className="bg-bec-color-bg-secundary h-16 flex items-center px-5 text-white">
       <nav className="w-full flex justify-between items-center">
         <div className="flex-1 flex justify-center">
-          <h1 className="text-lg sm:text-2xl ml-40">Beverages Ecommerce</h1>
+          <Link to="/" className="text-lg sm:text-2xl ml-40">
+            Beverages Ecommerce
+          </Link>
         </div>
         <div className="flex items-center">
           <div className="flex items-center mr-5">
-            <img src={cartIcon} alt="Cart" className="w-5 h-5" />
+            <Link to="/cart">
+              <img src={cartIcon} alt="Cart" className="w-5 h-5" />
+            </Link>
             <p className="hidden sm:block ml-1 text-sm sm:text-base">Cart</p>
           </div>
-          <a href="#" className="mr-5 text-sm sm:text-base ml-5">
+          <Link to="/login" className="mr-5 text-sm sm:text-base ml-5">
             Sign In
-          </a>
+          </Link>
         </div>
       </nav>
     </header>
