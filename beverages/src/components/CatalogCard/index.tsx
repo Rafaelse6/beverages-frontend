70df4f9c5a0fcb05df84b743ae6f8611bcd01430
Rafael Santos <rafa.05125@gmail.com>
2024-08-27 11:25:48 -0300

    Dynamically rendering beverage details with route parameter

--- beverages/src/components/CatalogCard/index.tsx
+++ beverages/src/components/CatalogCard/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { BeverageDTO } from "../../models/beverage";
 
 type Props = {
@@ -6,25 +7,27 @@ type Props = {
 
 export default function CatalogCard({ beverage }: Props) {
   return (
-    <div className="border border-bec-color-card-border rounded bg-bec-color-card-bg mb-4 ml-5 mr-5">
-      <div className="border-b border-bec-color-card-border p-4 flex justify-center">
-        <img
-          src={beverage.imgUrl}
-          alt={beverage.name}
-          className="max-w-full h-48 object-contain" // Ajuste de tamanho e proporção da imagem
-        />
+    <Link to={`/product-details/${beverage.id}`}>
+      <div className="border border-bec-color-card-border rounded bg-bec-color-card-bg mb-4 ml-5 mr-5">
+        <div className="border-b border-bec-color-card-border p-4 flex justify-center">
+          <img
+            src={beverage.imgUrl}
+            alt={beverage.name}
+            className="max-w-full h-48 object-contain" // Ajuste de tamanho e proporção da imagem
+          />
+        </div>
+        <div className="p-4">
+          <h3 className="text-bec-color-font-secondary text-xl font-bold flex items-center">
+            R$ {beverage.price.toFixed(2)}
+          </h3>
+          <h4 className="text-bec-color-font-primary text-lg font-semibold">
+            {beverage.name}
+          </h4>
+          <p className="text-bec-color-font-primary text-sm my-2.5 bg-gray-100 p-3 rounded-md">
+            {beverage.description}
+          </p>
+        </div>
       </div>
-      <div className="p-4">
-        <h3 className="text-bec-color-font-secondary text-xl font-bold flex items-center">
-          R$ {beverage.price.toFixed(2)}
-        </h3>
-        <h4 className="text-bec-color-font-primary text-lg font-semibold">
-          {beverage.name}
-        </h4>
-        <p className="text-bec-color-font-primary text-sm my-2.5 bg-gray-100 p-3 rounded-md">
-          {beverage.description}
-        </p>
-      </div>
-    </div>
+    </Link>
   );
 }
--- beverages/src/routes/ClientHome/ProductDetails/index.tsx
+++ beverages/src/routes/ClientHome/ProductDetails/index.tsx
@@ -1,37 +1,19 @@
+import { useParams } from "react-router-dom";
 import ButtonInverse from "../../../components/ButtonInverse";
 import ButtonPrimary from "../../../components/ButtonPrimary";
 import ProductDetailsCard from "../../../components/ProductDetailsCard";
-import { BeverageDTO } from "../../../models/beverage";
-
-const beverage: BeverageDTO = {
-  id: 2,
-  name: "Sprite",
-  description: "This soda is really good",
-  imgUrl:
-    "https://e7.pngegg.com/pngimages/79/169/png-clipart-sprite-easy-open-can-illustration-sprite-fizzy-drinks-coca-cola-fanta-beverage-can-fanta-food-aluminum-can.png",
-  price: 2.0,
-  categories: [
-    {
-      id: 1,
-      name: "Soda",
-    },
-    {
-      id: 3,
-      name: "Non alcoholic",
-    },
-    {
-      id: 4,
-      name: "Imported",
-    },
-  ],
-};
+import * as beverageService from "../../../services/beverage-service.ts";
 
 export default function ProductDetails() {
+  const params = useParams();
+
+  const beverage = beverageService.findById(Number(params.beverageId));
+
   return (
     <>
       <main className="bg-bec-color-bg-primary min-h-screen">
         <section id="product-details-section" className="dsc-container py-8">
-          <ProductDetailsCard beverage={beverage} />
+          {beverage && <ProductDetailsCard beverage={beverage} />}
           <div className="flex justify-center gap-4 mt-6">
             <ButtonPrimary text="BUY" />
             <ButtonInverse text="Start" />
